acaaa58f1262b27814b01e3548bd9f55f3bf1b92
MoamenKassem <moamenkassem@campus.technion.ac.il>
2023-05-19 14:26:34 +0300

    main is ready for testing

--- maina1.cpp
+++ maina1.cpp
@@ -16,6 +16,7 @@
 #include "StreamingDBa1.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 
 
 
@@ -27,44 +28,8 @@ void query_get_all_movies(string cmd, streaming_database *obj, Genre genre);
 
 int main()
 {
-    streaming_database *testing = new streaming_database();
-    testing->add_user(1,0);
-    testing->add_user(2,1);
-    testing->add_user(3,1);
-    testing->add_group(1);
-    testing->add_group(2);
-    testing->add_user_to_group(3,2);
-    testing->add_user_to_group(1,1);
-    testing->add_movie(1,Genre::COMEDY,10,1);
-    testing->add_movie(2, Genre::DRAMA,10,1);
-    testing->add_movie(3,Genre::FANTASY,10,1);
-    testing->add_movie(4,Genre::ACTION,10,1);
-    testing->add_movie(5,Genre::COMEDY,10,1);
-    testing->rate_movie(1,1,50);
-    testing->rate_movie(2,1,100);
-    testing->user_watch(2,1);
-    output_t<int> value = testing->get_num_views(2,Genre::COMEDY);
-    if(value.status()==StatusType::SUCCESS){
-        print("SUCCESS", value);
-    }else{ print("FAILED", value);}
-    testing->group_watch(2,1);
-    output_t<int> value1 = testing->get_num_views(2,Genre::COMEDY);
-    if(value1.status()==StatusType::SUCCESS){
-        print("SUCCESS", value1);
-    }else{ print("FAILED", value1);}
-    testing->user_watch(2,1);
-    testing->group_watch(1,1);
-    testing->add_user_to_group(2,1);
-    testing->remove_group(1);
-    output_t<int> value2 = testing->get_num_views(2,Genre::COMEDY);
-    if(value2.status()==StatusType::SUCCESS){
-        print("SUCCESS", value2);
-    }else{ print("FAILED", value2);}
-    testing->add_user_to_group(2,2);
-    output_t<int> value3 = testing->get_num_views(2,Genre::COMEDY);
-    if(value3.status()==StatusType::SUCCESS){
-        print("SUCCESS", value3);
-    }else{ print("FAILED", value3);}
+    std::ofstream myfile("C:/Users/LENOVO/Downloads/Data-Structures-Spring-2023-Wet-1/CurrentOutput.txt"); // Without append
+    myfile.open ("C:/Users/LENOVO/Downloads/Data-Structures-Spring-2023-Wet-1/CurrentOutput.txt",ios::app);
     int d1, d2, d3, g1;
     string b1;
     bool b;
@@ -85,7 +50,7 @@ int main()
                 b = false;
             }
             else {
-                cout << "Invalid input format" << endl;
+                myfile << "Invalid input format" << endl;
                 return -1;
             }
             print(op, obj->add_movie(d1, (Genre) g1, d2, b));
@@ -101,7 +66,7 @@ int main()
                 b = false;
             }
             else {
-                cout << "Invalid input format" << endl;
+                myfile << "Invalid input format" << endl;
                 return -1;
             }
             print(op, obj->add_user(d1, b));
@@ -139,12 +104,12 @@ int main()
             cin >> d1;
             print(op, obj->get_group_recommendation(d1));
         } else {
-            cout << "Unknown command: " << op << endl;
+            myfile << "Unknown command: " << op << endl;
             return -1;
         }
         // Verify no faults
         if (cin.fail()){
-            cout << "Invalid input format" << endl;
+            myfile << "Invalid input format" << endl;
             return -1;
         }
     }
@@ -165,20 +130,26 @@ static const char *StatusTypeStr[] =
 
 void print(string cmd, StatusType res)
 {
-    cout << cmd << ": " << StatusTypeStr[(int) res] << endl;
+    std::ofstream myfile;
+    myfile.open ("C:/Users/LENOVO/Downloads/Data-Structures-Spring-2023-Wet-1/CurrentOutput.txt",ios::app);
+    myfile << cmd << ": " << StatusTypeStr[(int) res] << endl;
 }
 
 void print(string cmd, output_t<int> res)
 {
+    std::ofstream myfile;
+    myfile.open ("C:/Users/LENOVO/Downloads/Data-Structures-Spring-2023-Wet-1/CurrentOutput.txt",ios::app);
     if (res.status() == StatusType::SUCCESS) {
-        cout << cmd << ": " << StatusTypeStr[(int) res.status()] << ", " << res.ans() << endl;
+        myfile << cmd << ": " << StatusTypeStr[(int) res.status()] << ", " << res.ans() << endl;
     } else {
-        cout << cmd << ": " << StatusTypeStr[(int) res.status()] << endl;
+        myfile << cmd << ": " << StatusTypeStr[(int) res.status()] << endl;
     }
 }
 
 void query_get_all_movies(string cmd, streaming_database *obj, Genre genre)
 {
+    std::ofstream myfile;
+    myfile.open ("C:/Users/LENOVO/Downloads/Data-Structures-Spring-2023-Wet-1/CurrentOutput.txt",ios::app);
     output_t<int> count = obj->get_all_movies_count(genre);
     int to_alloc = count.ans();
     if (to_alloc == 0)
@@ -199,7 +170,7 @@ void query_get_all_movies(string cmd, streaming_database *obj, Genre genre)
     if (status == StatusType::SUCCESS) {
         for (int i = 0; i < to_alloc; ++i)
         {
-            cout << out_mem[i] << endl;
+            myfile << out_mem[i] << endl;
         }
     }
     delete[] out_mem;
